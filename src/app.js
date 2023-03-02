fc9e8b094c03b6b9e5eee4276eed647c10c9cdf6
Sonjeongbeom <sjb990221@gmail.com>
2023-03-02 18:31:00 +0900

    Fix: package.json

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import express from 'express';
+
+const app = express('');
+const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('OK!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const server = app.listen(PORT, () => {
+  console.log(`server on ${PORT}`);
 });
