0e51f37e819cf3f223c50fd35d56f5c96555ab00
Sonjeongbeom <sjb990221@gmail.com>
2023-03-07 12:01:29 +0900

    Fix: transaction schema

--- src/api/transaction.model.js
+++ src/api/transaction.model.js
@@ -2,13 +2,15 @@ import mongoose, { Schema, Types } from 'mongoose';
 
 const TransactionSchema = new Schema({
   createdAt: { type: Date, defualt: Date.now },
-  transactionId: { type: Number, required: true },
-  userId: { type: String, required: true },
-  contractAmount: { type: Number, required: true },
+  transactionIdOfBtc: { type: Number, required: true },
+  clientOrderIdOfBtc: { type: String, required: true },
   numOfBtc: { type: Number, required: true },
   unitPriceOfBtc: { type: Number, required: true },
-  numOfEth: { type: Number, required: true },
+  transactionIdOfEth: { type: Number, required: true },
+  clientOrderIdOfEth: { type: String, required: true },
   unitPriceOfEth: { type: Number, required: true },
+  numOfEth: { type: Number, required: true },
+  contractAmount: { type: Number, required: true },
 });
 
 const TransactionModel = mongoose.model('transaction', TransactionSchema);
